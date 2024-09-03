25d42d861b299caa89750c25a513e0f6eea90d68
ItzApipAjalah <70223010+ItzApipAjalah@users.noreply.github.com>
2024-09-03 22:30:41 +0700

    add ads

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import LoadingScreen from '@/components/animation/LoadingScreen';
 import '@/app/globals.css';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import { AppProps } from 'next/app';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  
   const [loading, setLoading] = useState(true);
 
   const handleAnimationComplete = () => {
     setLoading(false);
   };
+
+  useEffect(() => {
+    // Run AdCash pop-up script after the component mounts
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.innerHTML = `
+      aclib.runPop({
+        zoneId: '8709442',
+      });
+
+      aclib.runAutoTag({
+        zoneId: 'dtiihq4n4k',
+    });
+    `;
+    document.body.appendChild(script);
+  }, []);
 
   return (
     <>
@@ -18,24 +33,17 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta charSet="UTF-8" />
         <link rel="icon" href="https://anime.amwp.website/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta
-          name="description"
-          content="my new portoflio using nextjs."
-        />
-        <meta
-          name="keywords"
-          content="portfolio, AMWP, afifmedya, ItzApipAjalah"
-        />
+        <meta name="description" content="my new portfolio using Next.js." />
+        <meta name="keywords" content="portfolio, AMWP, afifmedya, ItzApipAjalah" />
         <meta name="author" content="AMWP" />
         <meta property="og:title" content="Portfolio Afif Medya" />
-        <meta
-          property="og:description"
-          content="my new portoflio using nextjs."
-        />
+        <meta property="og:description" content="my new portfolio using Next.js." />
         <meta property="og:image" content="/image/logo.png" />
         <meta property="og:url" content="https://amwp.website" />
         <meta property="og:type" content="website" />
-        <title>Portofolio</title>
+        <title>Portfolio</title>
+        {/* AdCash Library Script */}
+        <script id="aclib" type="text/javascript" src="//acscdn.com/script/aclib.js"></script>
       </Head>
       {loading && <LoadingScreen onAnimationComplete={handleAnimationComplete} />}
       <div style={{ opacity: loading ? 0 : 1, transition: 'opacity 1s ease-in' }}>
